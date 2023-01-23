dfeb7964dddf181f6ea2c00a546f1b488e7198c3
Vinicius Bastos Clemente <vclemente2@hotmail.com>
2023-01-22 21:18:03 -0300

    Implementação da função de codificação

--- src/assets/javascript/index.js
+++ src/assets/javascript/index.js
@@ -1,1 +1,21 @@
+const codificador = {
+    chaveCodificadora: ['a', 'e', 'i', 'o', 'u'],
+    chaveDecodificadora: ['ai', 'enter', 'imes', 'ober', 'ufat'],
+    codifica: function (texto) {
+        let textoCodificado = '';
+        for (let letra of texto) {
+            for (let i = 0; i < this.chaveCodificadora.length; i++) {
+                const elemento = this.chaveCodificadora[i];
+                if (letra === elemento) {
+                    letra = this.chaveDecodificadora[i];
+                }
+            }
+            textoCodificado += letra;
+        }
+        return (textoCodificado);
+    },
+    decodifica: function (texto) {
 
+    }
+}
+codificador.codifica('aeiouaeiout');
