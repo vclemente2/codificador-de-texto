0cb29d9ba0978462d02a1efce8d897a1dd433902
Vinicius Bastos Clemente <vclemente2@hotmail.com>
2023-01-27 19:28:51 -0300

    Implementação do html e inicio da estilização com CSS.

---
+++ src/assets/javascript/script.js
@@ -0,0 +1,47 @@
+const codificador = {
+    chaveCodificadora: ['a', 'e', 'i', 'o', 'u'],
+    chaveDecodificadora: ['ai', 'enter', 'imes', 'ober', 'ufat'],
+    codifica: function () {
+        const { chaveCodificadora, chaveDecodificadora } = codificador;
+        let campoEntrada = document.querySelector("#entrada");
+        let campoSaida = document.querySelector("#saida");
+
+        let texto = campoEntrada.value;
+        let textoCodificado = '';
+
+        for (let letra of texto) {
+            for (let i = 0; i < chaveCodificadora.length; i++) {
+                const elemento = chaveCodificadora[i];
+                if (letra === elemento) {
+                    letra = chaveDecodificadora[i];
+                }
+            }
+            textoCodificado += letra;
+        }
+
+        campoSaida.value = textoCodificado;
+        campoEntrada.value = "";
+    },
+    decodifica: function () {
+        const { chaveCodificadora, chaveDecodificadora } = codificador;
+        let campoEntrada = document.querySelector("#entrada");
+        let campoSaida = document.querySelector("#saida");
+
+        let texto = campoEntrada.value;
+        let textoDecodificado = texto;
+
+        for (let i = 0; i < chaveDecodificadora.length; i++) {
+            const elemento = chaveDecodificadora[i];
+            while (textoDecodificado.includes(elemento)) {
+                textoDecodificado = textoDecodificado.replace(elemento, chaveCodificadora[i]);
+            }
+        }
+
+        campoSaida.value = textoDecodificado;
+        campoEntrada.value = "";
+    },
+    copiaTexto: function () {
+        let campoSaida = document.querySelector("#saida");
+        navigator.clipboard.writeText(campoSaida.value);
+    }
+}
