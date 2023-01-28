917815cfbe1bf360672a1a48e8b25264f25d44a9
Vinicius Bastos Clemente <vclemente2@hotmail.com>
2023-01-28 01:21:00 -0300

    Conclusão da estilização para desktop.

--- src/assets/javascript/script.js
+++ src/assets/javascript/script.js
@@ -21,6 +21,7 @@ const codificador = {
 
         campoSaida.value = textoCodificado;
         campoEntrada.value = "";
+        this.ajustaCampoSaida();
     },
     decodifica: function () {
         const { chaveCodificadora, chaveDecodificadora } = codificador;
@@ -43,5 +44,38 @@ const codificador = {
     copiaTexto: function () {
         let campoSaida = document.querySelector("#saida");
         navigator.clipboard.writeText(campoSaida.value);
+        alert("Texto copiado.");
+    },
+    ajustaCampoSaida: function () {
+        const campoSaida = document.querySelector("#saida");
+        const botaoCopia = document.querySelector("#botaoCopia");
+
+        if (campoSaida.value) {
+            campoSaida.style.cssText = "background-image: none;";
+            document.querySelector(".background-saida").remove();
+            botaoCopia.style.display = "block";
+        }
+    }
+}
+
+const controladorCorIconeRodape = {
+    corIcone: "222222",
+    alteraCorOn: function () {
+        this.corIcone = "0A3871";
+    },
+    alteraCorOff: function () {
+        this.corIcone = "222222"
+    },
+    alteraUrlLinkedin: function () {
+        const linkedin = document.querySelector("#linkedin");
+        linkedin.src = `https://img.shields.io/badge/LinkedIn-${this.corIcone}?style=for-the-badge&logo=linkedin&logoColor=white`;
+    },
+    alteraUrlEmail: function () {
+        const email = document.querySelector("#email");
+        email.src = `https://img.shields.io/badge/Gmail-${this.corIcone}?style=for-the-badge&logo=gmail&logoColor=white`;
+    },
+    alteraUrlGithub: function () {
+        const github = document.querySelector("#github");
+        github.src = `https://img.shields.io/badge/GitHub-${this.corIcone}?style=for-the-badge&logo=github&logoColor=white`;
     }
 }
